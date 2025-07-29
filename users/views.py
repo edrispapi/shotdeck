e5b1c16a81de29d747086c94079cb8bdb58a4aef
ادریس پاپی <64495523+edrispapi@users.noreply.github.com>
2025-07-29 03:30:40 +0330

    Create views.py
    ویوهای ثبتنام، ورود و پروفایل

---
+++ users/views.py
@@ -0,0 +1,32 @@
+from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .serializers import UserSerializer
+from rest_framework.response import Response
+
+User = get_user_model()
+
+class RegisterUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+class UserProfileView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
